6c36baca210dfa710ad89063d16215b46b69eaf9
GuilhermeFCF <guilherme.coucello@gmail.com>
2022-12-14 22:13:36 -0400

    Adicionado projetos android

--- GoogleMapsAct/app/src/main/java/com/example/googlemapsact/MapsActivity.java
+++ GoogleMapsAct/app/src/main/java/com/example/googlemapsact/MapsActivity.java
@@ -1,5 +1,6 @@
 package com.example.googlemapsact;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
@@ -8,6 +9,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.googlemapsact.databinding.ActivityMapsBinding;
@@ -44,8 +46,22 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
+        LatLng sydney = new LatLng(-8.763565, 30);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney,10));
+
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(@NonNull LatLng latLng) {
+                Double latitude = latLng.latitude;
+                Double longitude = latLng.longitude;
+                mMap.addMarker(new MarkerOptions().position(latLng).title("Local").snippet("Descrição").
+                        icon(
+                                BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)
+                        ));
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,12));
+            }
+        });
     }
+
 }
